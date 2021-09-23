b46ac59970bd5a70bb8e2223bf24ec9a9603875a
Alex Winikoff <ahw225@lehigh.edu>
2021-09-23 14:57:02 -0400

    Assignment 02

---
+++ Assignment 02B.R
@@ -0,0 +1,9 @@
+x<-10:1
+y<- -4:5
+z<-c('Hockey','Football','Curling','Soccer','Rugby','Baseball','Golf','Basketball','Wrestling','Tennis')
+theDF<-data.frame(x,y,z)
+
+# Editing names
+
+names(theDF)<-c("Popularity","Team Strength", "Sport")
+sd(theDF$"Team Strength")
